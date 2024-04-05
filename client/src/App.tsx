35c0d4c9abd95c3ad926c5685970720d192d2782
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-04-05 12:33:21 +0530

    create pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,23 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import SignIn from "./pages/SignIn.tsx";
+import SignUp from "./pages/SignUp.tsx";
+import Profile from "./pages/Profile.tsx";
+import About from "./pages/About.tsx";
 
 function App() {
 
   return (
-<></>
+      <BrowserRouter>
+
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
   )
 }
 
