00fca80b13e7fbfe00113c9ab00b1bffa4503235
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-05-14 23:51:49 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -8,9 +8,11 @@ dotenv.config();
 mongoose.connect(process.env.MONGO).then(r => {
     console.log('Connected to MongoDB!!');
 }).catch(e => {
-    console.log('Error connecting to MongoDB!!');
+    console.log('Error connecting to MongoDB!! Error: ', e);
 })
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!!');
 });
+
+
